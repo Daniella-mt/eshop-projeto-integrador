a0aafa81006dcb4eedcc5fb16cdf0bf7fbee5d60
Daniella-mt <151745713+Daniella-mt@users.noreply.github.com>
2023-11-30 16:03:06 -0300

    Update config.js
    test 5

--- src/axios/config.js
+++ src/axios/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchProducts = axios.create({
     // baseURL: 'https://fakestoreapi.com',
     // baseURL: 'https://andreasfreitag90.infinityfreeapp.com/php/teste',
-    baseURL: 'https://my-json-server.typicode.com/Daniella-mt/e-shop-api-react/produtos',
+    baseURL: 'https://my-json-server.typicode.com/Daniella-mt/e-shop-api-react',
     headers: {
         "Content-Type": "application/json"
     }
@@ -12,7 +12,7 @@ export const fetchProducts = axios.create({
 export const fetchCategories = axios.create({
     // baseURL: 'https://fakestoreapi.com/products/categories',
     // baseURL: 'https://andreasfreitag90.infinityfreeapp.com/php/teste',
-    baseURL: 'https://my-json-server.typicode.com/Daniella-mt/e-shop-api-react/produtos',
+    baseURL: 'https://my-json-server.typicode.com/Daniella-mt/e-shop-api-react',
     headers: {
         "Content-Type": "application/json"
     }
